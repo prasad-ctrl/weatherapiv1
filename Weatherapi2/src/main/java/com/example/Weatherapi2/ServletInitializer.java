ea6ce46e0a35db0b01849f62481f901f8f0fd5f5
prasad-ctrl <61504496+prasad-ctrl@users.noreply.github.com>
2024-09-25 20:40:13 +0530

    Add files via upload
    version 2 weather

---
+++ Weatherapi2/src/main/java/com/example/Weatherapi2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.Weatherapi2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Weatherapi2Application.class);
+	}
+
+}
---
+++ Weatherapi2/src/main/java/com/example/Weatherapi2/Weatherapi2Application.java
@@ -0,0 +1,13 @@
+package com.example.Weatherapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Weatherapi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Weatherapi2Application.class, args);
+	}
+
+}
