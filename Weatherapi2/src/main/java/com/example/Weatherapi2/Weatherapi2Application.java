fe594194e70ea68fe8ac8d4bbadb02d5f7056016
Renuka Prasad <rprasad7999@gmail.com>
2024-09-25 20:44:02 +0530

    intial

--- Weatherapi2/src/main/java/com/example/Weatherapi2/Weatherapi2Application.java
+++ Weatherapi2/src/main/java/com/example/Weatherapi2/Weatherapi2Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Weatherapi2Application {
 
 	public static void main(String[] args) {
+
+
+		System.out.println(" we");
 		SpringApplication.run(Weatherapi2Application.class, args);
 	}
 
